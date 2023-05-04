38beb54deb1e9299a57046c93bec42676aecce50
your-name <your-email-address>
2023-05-05 00:09:28 +0300

    create signup and login user api

--- users/views.py
+++ users/views.py
@@ -7,10 +7,7 @@ from users.service import create_user, reset_password_email_send, login_user, re
 class SignUpApi(Resource):
     @staticmethod
     def post() -> Response:
-        """
-        POST response method for creating user.
-        :return: JSON object
-        """
+   
         input_data = request.get_json()
         response, status = create_user(request, input_data)
         return make_response(response, status)
@@ -19,10 +16,7 @@ class SignUpApi(Resource):
 class LoginApi(Resource):
     @staticmethod
     def post() -> Response:
-        """
-        POST response method for login user.
-        :return: JSON object
-        """
+        
         input_data = request.get_json()
         response, status = login_user(request, input_data)
         return make_response(response, status)
@@ -31,10 +25,7 @@ class LoginApi(Resource):
 class ForgotPassword(Resource):
     @staticmethod
     def post() -> Response:
-        """
-        POST response method for forgot password email send user.
-        :return: JSON object
-        """
+      
         input_data = request.get_json()
         response, status = reset_password_email_send(request, input_data)
         return make_response(response, status)
@@ -43,10 +34,7 @@ class ForgotPassword(Resource):
 class ResetPassword(Resource):
     @staticmethod
     def post(token) -> Response:
-        """
-        POST response method for save new password.
-        :return: JSON object
-        """
+       
         input_data = request.get_json()
         response, status = reset_password(request, input_data, token)
         return make_response(response, status)
