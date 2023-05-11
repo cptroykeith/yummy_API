728fdc02252bb75abe3d867995640e85d5acbcf9
your-name <katongole.roy100@gmail.com>
2023-05-12 02:12:44 +0300

    data validation

--- users/validation.py
+++ users/validation.py
@@ -1,11 +1,12 @@
 from marshmallow import Schema, fields, validate
 
 
+
 class CreateSignupInputSchema(Schema):
     # the 'required' argument ensures the field exists
-    username = fields.Str(required=True, validate=validate.Length(min=4))
+    username = fields.Str(required=True, validate=[validate.Length(min=4), validate.Regexp(r'^\S.*\S$'), validate.Regexp(r'^\w+$', error="Username must be one word")])
     email = fields.Email(required=True)
-    password = fields.Str(required=True, validate=validate.Length(min=6))
+    password = fields.Str(required=True, validate=[validate.Length(min=6), validate.Regexp(r'^\w+$')])
 
 
 class CreateLoginInputSchema(Schema):
