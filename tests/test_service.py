3b6bd1c5e4a42ae50e922dc5d71a1ce58d70d57f
your-name <katongole.roy100@gmail.com>
2023-05-25 21:54:26 +0300

    test_get_all_users

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,7 +1,8 @@
 import pytest
 from server import create_app
-from users.service import create_user, login_user
-from utils.http_code import HTTP_201_CREATED
+from users.service import create_user, get_all_users, login_user
+from utils.http_code import HTTP_200_OK, HTTP_201_CREATED
+
 
 @pytest.fixture
 def client():
@@ -34,5 +35,34 @@ def test_login_user(client):
     assert response[0].get("message"),"User login successfully" == "User login successfully"
     assert response[0].get("status"), HTTP_201_CREATED == HTTP_201_CREATED
 
-def get_all_users():
-    
+
+
+def test_get_all_users(client):
+    # Create some test users
+    create_user(None, {
+        "username": "user1",
+        "email": "user1@example.com",
+        "password": "password1"
+    })
+    create_user(None, {
+        "username": "user2",
+        "email": "user2@example.com",
+        "password": "password2"
+    })
+
+    # Call the get_all_users endpoint
+    response, status_code = get_all_users(None)
+
+    # Assert the response
+    assert response.status_code == HTTP_200_OK
+    assert response.json["message"] == "All users retrieved"
+    assert len(response.json["data"]) == 7
+
+    # Assert the user data
+    user1 = response.json["data"][0]
+    assert user1["username"] == "royw"
+    assert user1["email"] == "roy@gmail.com"
+
+    user2 = response.json["data"][1]
+    assert user2["username"] == "steby"
+    assert user2["email"] == "steby@gmail.com"
